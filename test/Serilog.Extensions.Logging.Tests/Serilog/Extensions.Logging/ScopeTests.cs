88244a8aabdbac14fef09c1a1b4a26d1a0ff16dc
huoshan12345 <huoshan12345@hotmail.com>
2025-03-31 08:04:59 +0800

    initial

---
+++ test/Serilog.Extensions.Logging.Tests/Serilog/Extensions.Logging/ScopeTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Serilog.Context;
+using Serilog.Debugging;
+using Serilog.Events;
+
+namespace Serilog.Extensions.Logging;
+
+public class ScopeTests
+{
+    [Fact]
+    public async Task PropertyOverride_Scope()
+    {
+        var listener = new LogEventListener();
+
+        var serilog = new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .WriteTo.Sink(listener)
+            .CreateLogger();
+
+        var provider = new ServiceCollection()
+            .AddLogging(m => m.AddSerilog(serilog, dispose: true))
+            .BuildServiceProvider();
+
+        var logger = provider.GetRequiredService<ILogger<ScopeTests>>();
+
+        using (logger.BeginScope(new Dictionary<string, object?> { { "Value", 1 } }))
+        using (logger.BeginScope(new Dictionary<string, object?> { { "Value", 2 } }))
+        {
+            logger.LogInformation("Value: {Value}", 3);
+        }
+
+        Assert.True(await listener.WaitAsync(1, TimeSpan.FromSeconds(3)));
+
+        var logEvent = listener.Events.First();
+        var value = (logEvent.Properties["Value"] as ScalarValue)?.Value;
+
+        // since Serilog.Extensions.Logging 9.0.1
+        Assert.Equal(2, value);
+
+        // before Serilog.Extensions.Logging 9.0.1
+        // Assert.Equal(3, value);
+    }
+
+    [Fact]
+    public async Task PropertyOverride_LogContext()
+    {
+        var listener = new LogEventListener();
+
+        var logger = new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .WriteTo.Sink(listener)
+            .CreateLogger();
+
+        using (LogContext.PushProperty("Value", 1))
+        using (LogContext.PushProperty("Value", 2))
+        {
+            logger.Information("Value: {Value}", 3);
+        }
+
+        Assert.True(await listener.WaitAsync(1, TimeSpan.FromSeconds(3)));
+
+        var logEvent = listener.Events.First();
+        Assert.True(logEvent.Properties.TryGetValue("Value", out var value));
+        Assert.Equal(3, (value as ScalarValue)?.Value);
+    }
+}
---
+++ test/Serilog.Extensions.Logging.Tests/Serilog/LogEventListener.cs
@@ -0,0 +1,27 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+
+namespace Serilog;
+
+public class LogEventListener : ILogEventSink
+{
+    private readonly SemaphoreSlim _semaphore = new(0);
+    public List<LogEvent> Events { get; } = [];
+
+    public virtual void Emit(LogEvent logEvent)
+    {
+        Events.Add(logEvent);
+        _semaphore.Release();
+    }
+
+    public async Task<bool> WaitAsync(int count, TimeSpan timeout, CancellationToken cancellationToken = default)
+    {
+        for (var i = 0; i < count; ++i)
+        {
+            var flag = await _semaphore.WaitAsync(timeout, cancellationToken);
+            if (flag == false)
+                return false; // timeout
+        }
+        return true;
+    }
+}
