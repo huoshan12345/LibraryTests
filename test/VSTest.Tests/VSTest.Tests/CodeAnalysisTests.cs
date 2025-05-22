b6851d380a08b7ff4184fb9508eefbbab19b4a71
huoshan12345 <huoshan12345@hotmail.com>
2025-05-23 06:49:44 +0800

    x

---
+++ test/VSTest.Tests/VSTest.Tests/CodeAnalysisTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Xunit;
+
+namespace VSTest.Tests;
+
+public class CodeAnalysisTests
+{
+    [Fact]
+    public void GetCustomAttributes_Test()
+    {
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+        foreach (var assembly in assemblies)
+        {
+            foreach (var type in assembly.GetExportedTypes())
+            {
+                foreach (var property in type.GetProperties())
+                {
+                    //  Could not load type 'System.Diagnostics.CodeAnalysis.MemberNotNullWhenAttribute' from assembly 'Microsoft.TestPlatform.CoreUtilities'
+                    var attributes = property.GetCustomAttributes();
+                }
+            }
+        }
+    }
+}
